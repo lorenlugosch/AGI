a67f01961cda8db04d2cee9407f93e0324f42c4e
loren.lugosch <loren.lugosch@gmail.com>
2022-10-28 20:21:26 -0500

    add Transducer modules, implement encoder and predictor processing

--- train-AGI.py
+++ train-AGI.py
@@ -7,18 +7,107 @@ from hyperpyyaml import load_hyperpyyaml
 import struct
 import torchvision
 
-conv = torch.nn.Conv2d(in_channels=3, out_channels=128, stride=7, kernel_size=7)
+# conv = torch.nn.Conv2d(in_channels=3, out_channels=128, stride=7, kernel_size=7)
+
+encoder_dim = 1024
+predictor_dim = 1024
+joiner_dim = 1024
+audio_dim = 80
+audio_frames_per_video_frame = 10
+
+# adapted from https://github.com/lorenlugosch/transducer-tutorial/blob/main/transducer_tutorial_example.ipynb
+class Encoder(torch.nn.Module):
+	def __init__(self):
+		super(Encoder, self).__init__()
+		self.combine = torch.nn.Linear(audio_dim*audio_frames_per_video_frame + 128, encoder_dim)
+		# self.audio_encoder = 
+		self.video_encoder = torch.nn.Conv2d(in_channels=3, out_channels=128, stride=7, kernel_size=7)
+		self.rnn = torch.nn.LSTM(input_size=encoder_dim, hidden_size=encoder_dim, num_layers=3, batch_first=True, bidirectional=False, dropout=0.15)
+		self.linear = torch.nn.Linear(encoder_dim, joiner_dim)
+
+	def forward(self, audio,video):
+		batch_size = video.shape[0]
+		video_T = video.shape[1]
+		audio_features = audio #self.audio_encoder(audio)
+		audio_T = audio_features.shape[1]
+		pad_ = (video_T*audio_frames_per_video_frame) - audio_T
+		audio_features = torch.nn.functional.pad(audio_features, (0,0,0,pad_))
+		audio_features = audio_features.reshape(batch_size, video_T, audio_dim*audio_frames_per_video_frame)
+
+		video = video.reshape(-1, 3, 630, 300)
+		video_features = self.video_encoder(video)
+		video_features = video_features.max(dim=2)[0].max(dim=2)[0]
+		video_features = video_features.reshape(batch_size, video_T, 128)
+		out = torch.cat([audio_features, video_features], dim=2)
+		out = self.combine(out)
+		out = self.rnn(out)[0]
+		out = self.linear(out)
+		return out
+
+class Predictor(torch.nn.Module):
+	def __init__(self, output_dim):
+		super(Predictor, self).__init__()
+		# self.embed = torch.nn.Embedding(output_dim, predictor_dim)
+		self.embed = torch.nn.Linear(output_dim, predictor_dim)
+		self.rnn = torch.nn.GRUCell(input_size=predictor_dim, hidden_size=predictor_dim)
+		self.linear = torch.nn.Linear(predictor_dim, joiner_dim)
+		self.initial_state = torch.nn.Parameter(torch.randn(predictor_dim))
+		self.start_token = torch.tensor([-1, -1, -1.])
+
+	def forward_one_step(self, input, previous_state):
+		embedding = self.embed(input)
+		state = self.rnn.forward(embedding, previous_state)
+		out = self.linear(state)
+		return out, state
+
+	def forward(self, y):
+		batch_size = y.shape[0]
+		U = y.shape[1]
+		outs = []
+		state = torch.stack([self.initial_state] * batch_size) #.to(y.device)
+		for u in range(U+1): # need U+1 to get null output for final timestep 
+			if u == 0:
+				decoder_input = torch.stack([self.start_token] * batch_size) #.to(y.device)
+			else:
+				decoder_input = y[:,u-1]
+			out, state = self.forward_one_step(decoder_input, state)
+			outs.append(out)
+		out = torch.stack(outs, dim=1)
+		return out
+
+class Joiner(torch.nn.Module):
+	def __init__(self, num_outputs):
+		super(Joiner, self).__init__()
+		self.linear = torch.nn.Linear(joiner_dim, num_outputs)
+
+	def forward(self, encoder_out, predictor_out):
+		out = encoder_out + predictor_out
+		out = torch.nn.functional.relu(out)
+		out = self.linear(out)
+		return out
 
 class AGI(sb.Brain):
 	def compute_forward(self, batch, stage):
-		# print(batch.sig[0].shape)
-		vid = batch.vid[0].reshape(-1, 3, 630, 300)
-		print(batch.unaligned_gestures[0].shape)
-		print(batch.aligned_gestures[0].shape)
-		return self.modules.conv(vid)
+		batch = batch.to(self.device)
+		video = batch.vid[0]
+		audio,audio_lens = batch.sig
+		audio = self.hparams.compute_features(audio)
+		audio = self.modules.normalize(audio,audio_lens)
+		encoder_out = self.modules.encoder(audio,video)
+
+		unaligned_gestures = batch.unaligned_gestures[0]
+		batch_size = unaligned_gestures.shape[0]
+		# start_pad = torch.stack([self.modules.predictor.start_token] * batch_size).unsqueeze(1)
+		# unaligned_gestures_in = torch.cat([start_pad, unaligned_gestures], dim=1)
+		predictor_out = self.modules.predictor(unaligned_gestures)
+
+		aligned_gestures = batch.aligned_gestures[0]
+		return joiner_out
+
 	def compute_objectives(self, predictions, batch, stage):
-		vid = batch.vid[0].reshape(-1, 3, 630, 300)
-		return torch.nn.functional.l1_loss(predictions, predictions) #blah)
+		joiner_out = predictions
+		return torch.nn.functional.l1_loss(joiner_out, joiner_out)
+
 	def on_stage_end(self, stage, stage_loss, epoch):
 		print("yay!")
 
@@ -40,8 +129,7 @@ def prepare_data(hparams):
 	@sb.utils.data_pipeline.takes("screens")
 	@sb.utils.data_pipeline.provides("vid")
 	def screens_pipeline(screens):
-		video_path = data_folder + "/" + screens
-		vid = torchvision.io.read_video(video_path, output_format="TCHW")[0].float() / 255.
+		vid = torchvision.io.read_video(data_folder + "/" + screens, output_format="TCHW")[0].float() / 255.
 		return vid
 	sb.dataio.dataset.add_dynamic_item(datasets, screens_pipeline)
 
@@ -132,24 +220,8 @@ with open(hparams_file) as fin:
 train_data = prepare_data(hparams)
 
 # modules = {"model": torch.nn.Linear(in_features=10 , out_features=10) }
-modules = {"conv": conv }
-brain = AGI(modules, lambda x : torch.optim.SGD(x, 0.1) )
-# data = [{"input": np.random.rand(10 , 10).astype("float32") , "target": np.random.rand(10, 10).astype("float32") }]
+modules = {"encoder": Encoder(), "predictor": Predictor(output_dim=3), "joiner": Joiner(num_outputs=4), "normalize": sb.processing.features.InputNormalization(norm_type="global")}
+brain = AGI(modules, lambda x: torch.optim.SGD(x, 0.1), hparams=hparams)
 brain.fit(epoch_counter=range(15), train_set=train_data, train_loader_kwargs=hparams["train_dataloader_opts"])
 
-
-
-# # type a 1 in the calculator
-# adb shell sendevent /dev/input/event2 3 57 0; adb shell sendevent /dev/input/event2 3 53 3253; adb shell sendevent /dev/input/event2 3 54 21553; adb shell sendevent /dev/input/event2 3 58 1024; adb shell sendevent /dev/input/event2 0 0 0;   adb shell sendevent /dev/input/event2 3 58 0;  adb shell sendevent /dev/input/event2 3 57 -1; adb shell sendevent /dev/input/event2 0 0 0
-# adb shell sendevent /dev/input/event2 3 57 0; adb shell sendevent /dev/input/event2 3 53 3253; adb shell sendevent /dev/input/event2 3 54 21553; adb shell sendevent /dev/input/event2 3 58 1024; adb shell sendevent /dev/input/event2 0 0 0;   adb shell sendevent /dev/input/event2 3 58 0;  adb shell sendevent /dev/input/event2 3 57 -1; adb shell sendevent /dev/input/event2 3 53 3253; adb shell sendevent /dev/input/event2 3 54 21553; adb shell sendevent /dev/input/event2 0 0 0
-# adb shell sendevent /dev/input/event2 3 57 0; adb shell sendevent /dev/input/event2 3 53 3253; adb shell sendevent /dev/input/event2 3 54 21553; adb shell sendevent /dev/input/event2 3 58 1024; adb shell sendevent /dev/input/event2 0 0 0;  adb shell sendevent /dev/input/event2 3 57 -1; adb shell sendevent /dev/input/event2 3 53 3253; adb shell sendevent /dev/input/event2 3 54 21553;  adb shell sendevent /dev/input/event2 3 58 0; adb shell sendevent /dev/input/event2 0 0 0
-
-# (1666202942, 553795, 3, 57, 0)
-# (1666202942, 553795, 3, 53, 3253)
-# (1666202942, 553795, 3, 54, 21553)
-# (1666202942, 553795, 3, 58, 1024)
-# (1666202942, 553795, 0, 0, 0)
-# (1666202943, 76135, 3, 58, 0)
-# (1666202943, 76135, 3, 57, -1)
-# (1666202943, 76135, 0, 0, 0)
 
